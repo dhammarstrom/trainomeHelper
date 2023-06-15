6763428ac59db2f682c866547d1a1a220e0b60b1
dhammarstrom <d.hammarstrom@hotmail.se>
2023-06-15 14:30:42 +0200

    next step document tutorial

--- R/seq_wrapper.R
+++ R/seq_wrapper.R
@@ -1,15 +1,3 @@
-
-
-dat <- data.frame(time = rep(c(0,1), each = 10),
-                   count = rnbinom(20, size = 20, mu = 10),
-
-
-
-
-                                    id = rep(seq(1:10)))
-
-
-
 #' A function to fit models with a chosen fitting algorithm. To be used in seq_wrapper.
 #' @param FUN Name of a fitting function, like glmmTMB::glmmTMB
 #' @param ARG A lsit of arguments that can be evaluated by the fitting function
@@ -27,7 +15,55 @@ fit_fun <- function(FUN, ARG) {
 }
 
 
+#' Transform, merge and fit models. The function is used inside seq_wrapper to combine
+#' metadata with target-level data and perform the model fitting. The function is used in a
+#' call to pbapply::pblapply.
+#' @param x A list of target-specific counts
+#' @param samp_name Sample names from the upper level function
+#' @param arg_list Arguments from the upper level function
+#' @param ffun the fitting function from the upper level function
+transform_merge_fit <- function(x,
+                                samp_name = samplename,
+                                metdat = metadata,
+                                arg_list = arguments,
+                                add_vars = additional_vars,
+                                ffun = fitting_fun) {
 
+  transposed <- data.frame(seq_sample_id = rownames(t(x[,-1])), counts = as.numeric(t(x[,-1])))
+
+  colnames(transposed)[1] <- samp_name
+
+  df <- merge(transposed, data.frame(metdat), by = samp_name)
+
+  ## Keep only data needed for fitting
+  if("formula" %in% names(arg_list))  parsed <- all.vars(as.formula(arg_list$formula))
+  if("model" %in% names(arg_list))  parsed <- all.vars(as.formula(arg_list$model))
+  if(all(c("fixed", "random") %in% names(arg_list)))  parsed <- c(all.vars(as.formula(arg_list$fixed)), all.vars((arg_list$random[[1:length(arg_list$random)]])))
+
+  ## Keep also additional variables that exists in the meta data data set
+  if(!is.null(add_vars)) parsed <- c(parsed, add_vars)
+
+
+  df <- df[,parsed, drop = FALSE]
+
+
+  ## Remove attributes from the list of arguments (this solves an issue when using glmmTMB)
+
+
+  arguments_final <- append(arguments, list(data = df))
+
+for(i in 1:length(arguments_final)) {
+
+  if(class(arguments_final[[i]]) == "formula") environment(arguments_final[[i]]) <- NULL
+
+}
+
+
+  model <- fit_fun(ffun, arguments_final)
+
+  return(model)
+
+}
 
 
 
@@ -41,25 +77,26 @@ fit_fun <- function(FUN, ARG) {
 #' and design variables.
 #' @param samplename A character value indicating the variable by which metadata can merge with the target data.
 #' This defaults to "seq_sample_id" as this is used in the trainomeMetaData package.
+#' @param additional_vars A vector of additional variables that is contained in the metadata data set that is needed to fit the model. By default the metadata is reduced to variables contained in the slots formula/model/fixed/random in additional arguments. More variables may be needed for offsets, weights etc.
 #' @param summary_fun A custom (user-created) function for evaluating/summarizing models. If NULL, a list of fitted models are returned
 #' @param eval_fun A custom (user-created) function for model diagnostics/evaluation. If NULL, no evaluation/diagnostics of models are returned
 #' @param exported A list of functions, values etc. to be passed to summary_fun and eval_fun. This list must contain any functions that should be used in model summarise or evaluations.
-#' @param cores An integer indicating the number of cores to be used in parallel computations. If NULL, a sequential for loop is used. If "max", all available cores are used
-#' @return A nested list with three upper levels slots: models, a list of fitted objects; summaries, a list of summaries created from the summary_fun function; diagnostics, a list of diagnostics created from eval_fun
-#' @details
+#' @param subset A sequence, random samples or integers to indicate which rows to keep in data. This is useful if you want to test the model in a subset of targets. If keft to the default (NULL), all rows will be used.
+#' @param cores An integer indicating the number of cores to be used in parallel computations. If NULL, a sequential for loop is used. If "max", all available cores are used.
+#' @return A nested list with three upper levels slots: models, a list of fitted objects; summaries, a list of summaries created from the summary_fun function; evaluations, a list of diagnostics created from eval_fun.
+#' @details This function provides a flexible wrapper to fit, summarize and evaluate statistical models fitted to high dimensional omics-type data.
 #' @export
-#'
 seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
                         arguments,
                         data,
                         metadata,
                         samplename = "seq_sample_id",
+                        additional_vars = NULL,
                         summary_fun = NULL,
                         eval_fun = NULL,
                         exported = list(),
+                        subset = NULL,
                         cores) {
-
-
 
   ## Sanity checks
 
@@ -75,6 +112,13 @@ seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
 
 
   ## Prepare data #####
+  data <- data.frame(data)
+  metadata <- data.frame(metadata)
+
+  ## Subset
+  if(!is.null(subset)) data <- data[subset,]
+
+
 
   ## Count tables from the trainomeMetaData package comes with the first column
   # being trainscript id's. This splits the data into data frames based on number of
@@ -84,68 +128,31 @@ seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
 
 
   # To merge meta data with meta data and fit the model, return model object
-  transform_merge_fit <- function(x,
-                                  samp_name = samplename,
-                                  metdat = metadata,
-                                  arg_list = arguments,
-                                  ffun = fitting_fun) {
-
-    transposed <- data.frame(seq_sample_id = rownames(t(x)), counts = as.numeric(t(x)))
-
-    colnames(transposed)[1] <- samp_name
-
-    df <- merge(transposed, data.frame(metdat), by = samp_name)
-
-    ## Keep only data needed for fitting
-    if("formula" %in% names(arg_list))  parsed <- all.vars(as.formula(arg_list$formula))
-    if("model" %in% names(arg_list))  parsed <- all.vars(as.formula(arg_list$model))
-    if(all(c("fixed", "random") %in% names(arg_list)))  parsed <- c(all.vars(as.formula(arg_list$fixed)), all.vars((arg_list$random[[1:length(arg_list$random)]])))
-
-
-    df <- df[,parsed, drop = FALSE]
-
-
-    ## Remove attributes from the list of arguments
-    one_entry <- function(x) {
-      for (i in length(x)) attr(x[[i]], "names") <- NULL
-      environment(x) <- NULL
-      return(x)
-    }
-
-    arguments_final <- append(arg_list, list(data = df))
-    arguments_final_noattr <- lapply(arguments_final, one_entry)
-
-
-
-    model <- fit_fun(ffun, arguments_final_noattr)
-
-    return(model)
-
-  }
 
   ### Fitting models in parallel #######
 
   # Determine the number of cores
   if(is.null(cores)) num_cores <- 1
   if(cores >= parallel::detectCores()) num_cores <- parallel::detectCores()
-  if(cores < parallel::detectCores()) num_cores <- cores
+  if(cores <= parallel::detectCores()) num_cores <- cores
 
 
   # Create a cluster using the number of cores specified
   cl <- parallel::makeCluster(num_cores)
 
   ## Export data to clusters
-  parallel::clusterExport(cl, c("metadata",
+  parallel::clusterExport(cl, varlist = c("metadata",
                                 "arguments",
                                 "fit_fun",
                                 "samplename",
-                                "fitting_fun"))
+                                "fitting_fun"),
+                          envir = environment())
 
 
 
   # Parallel execution of the fitting process
   cat("Transforming, merging and modelling data.\n")
-  model_fits <- pbapply::pblapply(cl = cl, X = count_dfs, FUN = transform_merge_fit)
+  model_fits <- pbapply::pblapply(cl = cl, X = count_dfs, FUN = trainomeHelper:::transform_merge_fit)
 
 
   parallel::stopCluster(cl)
@@ -166,7 +173,7 @@ seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
     cl2 <- parallel::makeCluster(num_cores)
 
     ## Export data to clusters
-    parallel::clusterExport(cl2, c("exported"))
+    parallel::clusterExport(cl2, varlist = c("exported"), envir = environment())
 
     # Using the user provided function
 
@@ -185,7 +192,7 @@ seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
     cl3 <- parallel::makeCluster(num_cores)
 
     ## Export data to clusters
-    parallel::clusterExport(cl3, c("exported"))
+    parallel::clusterExport(cl3, varlist = c("exported"), envir = environment())
 
     # Using user-provided function
     results$model_evaluations <- pbapply::pblapply(cl = cl3, X = model_fits, FUN = eval_fun)
@@ -203,50 +210,3 @@ seq_wrapper <- function(fitting_fun = glmmTMB::glmmTMB,
 }
 
 
-
-custom_sum <- function(x) {
-
-  res <- data.frame(marginaleffects::avg_comparisons(x,
-                                   variables = list(time = c("w0", "w2pre"))))
-
-
-
-  return(res)
-
-}
-
-
-
-
-
-
-
-
-##
-tdf <- tiny_dat %>%
-  mutate_if(is.numeric, round) %>%
-  mutate_if(is.numeric, as.integer) %>%
-  print()
-
-
-glmmTMB::glmmTMB(formula = counts ~ time, family = glmmTMB)
-
-
-
-fits <- seq_wrapper(fitting_fun = glmmTMB::glmmTMB,
-                        arguments = list(formula =counts ~ time + (1|participant), family = glmmTMB::nbinom2(link = "log")),
-                        data = data.frame(tdf),
-                        metadata = data.frame(metadata),
-                        samplename = "seq_sample_id",
-                    summary_fun = custom_sum,
-                    exported = list(marginaleffects::avg_comparisons),
-                        cores = 4)
-
-
-bind_rows(fits$model_summarise) %>%
-  mutate(transcript = names(fits$model_summarises)) %>%
-  ggplot(aes(p.value)) + geom_histogram()
-
-
-
-fits$model_summarises
---
+++ data/rna-seq-sample-data.R
@@ -0,0 +1,23 @@
+
+# A small RNA-seq data set used for illustrations in the vignette and testing.
+#
+
+## Using trainomeMetaData
+library(trainomeMetaData); library(tidyverse)
+
+## A complete data se
+compl_data <- read_csv("ome-data/vol_transcript_rsem.csv")
+
+# Keep expressed transcripts
+compl_data <- compl_data[rowSums(compl_data[,-1]) > 100,]
+
+# Sample 100 rows
+rna_seq_sample <- compl_data[sample(1:nrow(compl_data), 1000), ]
+
+
+usethis::use_data(rna_seq_sample)
+
+## Metadata
+rna_seq_metadata <- vol_samples
+
+usethis::use_data(rna_seq_metadata)
